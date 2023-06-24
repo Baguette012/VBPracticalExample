1c048885b87c691ab431bb55d5f2df4c98ac2ae4
Baguette012 <137230559+Baguette012@users.noreply.github.com>
2023-06-24 16:34:12 -0400

    Add files via upload

---
+++ HW3Objectss/HW3Objectss/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnSave = New System.Windows.Forms.Button()
+        Me.BtnLoad = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LblAverage = New System.Windows.Forms.Label()
+        Me.LblTotalItems = New System.Windows.Forms.Label()
+        Me.lblTotalSales = New System.Windows.Forms.Label()
+        Me.BtnSubmit = New System.Windows.Forms.Button()
+        Me.TxtInput = New System.Windows.Forms.TextBox()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.LblName = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnSave
+        '
+        Me.BtnSave.Location = New System.Drawing.Point(422, 182)
+        Me.BtnSave.Name = "BtnSave"
+        Me.BtnSave.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSave.TabIndex = 16
+        Me.BtnSave.Text = "Save"
+        Me.BtnSave.UseVisualStyleBackColor = True
+        '
+        'BtnLoad
+        '
+        Me.BtnLoad.Location = New System.Drawing.Point(422, 154)
+        Me.BtnLoad.Name = "BtnLoad"
+        Me.BtnLoad.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLoad.TabIndex = 15
+        Me.BtnLoad.Text = "Load"
+        Me.BtnLoad.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(238, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Insert Price"
+        '
+        'LblAverage
+        '
+        Me.LblAverage.AutoSize = True
+        Me.LblAverage.Location = New System.Drawing.Point(434, 297)
+        Me.LblAverage.Name = "LblAverage"
+        Me.LblAverage.Size = New System.Drawing.Size(0, 13)
+        Me.LblAverage.TabIndex = 13
+        '
+        'LblTotalItems
+        '
+        Me.LblTotalItems.AutoSize = True
+        Me.LblTotalItems.Location = New System.Drawing.Point(365, 297)
+        Me.LblTotalItems.Name = "LblTotalItems"
+        Me.LblTotalItems.Size = New System.Drawing.Size(13, 13)
+        Me.LblTotalItems.TabIndex = 12
+        Me.LblTotalItems.Text = "1"
+        '
+        'lblTotalSales
+        '
+        Me.lblTotalSales.AutoSize = True
+        Me.lblTotalSales.Location = New System.Drawing.Point(304, 297)
+        Me.lblTotalSales.Name = "lblTotalSales"
+        Me.lblTotalSales.Size = New System.Drawing.Size(13, 13)
+        Me.lblTotalSales.TabIndex = 11
+        Me.lblTotalSales.Text = "0"
+        '
+        'BtnSubmit
+        '
+        Me.BtnSubmit.Location = New System.Drawing.Point(304, 182)
+        Me.BtnSubmit.Name = "BtnSubmit"
+        Me.BtnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSubmit.TabIndex = 10
+        Me.BtnSubmit.Text = "Submit"
+        Me.BtnSubmit.UseVisualStyleBackColor = True
+        '
+        'TxtInput
+        '
+        Me.TxtInput.Location = New System.Drawing.Point(304, 156)
+        Me.TxtInput.Name = "TxtInput"
+        Me.TxtInput.Size = New System.Drawing.Size(100, 20)
+        Me.TxtInput.TabIndex = 9
+        '
+        'TxtName
+        '
+        Me.TxtName.Location = New System.Drawing.Point(307, 130)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.Size = New System.Drawing.Size(100, 20)
+        Me.TxtName.TabIndex = 17
+        '
+        'LblName
+        '
+        Me.LblName.AutoSize = True
+        Me.LblName.Location = New System.Drawing.Point(238, 130)
+        Me.LblName.Name = "LblName"
+        Me.LblName.Size = New System.Drawing.Size(64, 13)
+        Me.LblName.TabIndex = 18
+        Me.LblName.Text = "Insert Name"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LblName)
+        Me.Controls.Add(Me.TxtName)
+        Me.Controls.Add(Me.BtnSave)
+        Me.Controls.Add(Me.BtnLoad)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LblAverage)
+        Me.Controls.Add(Me.LblTotalItems)
+        Me.Controls.Add(Me.lblTotalSales)
+        Me.Controls.Add(Me.BtnSubmit)
+        Me.Controls.Add(Me.TxtInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnSave As Button
+    Friend WithEvents BtnLoad As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LblAverage As Label
+    Friend WithEvents LblTotalItems As Label
+    Friend WithEvents lblTotalSales As Label
+    Friend WithEvents BtnSubmit As Button
+    Friend WithEvents TxtInput As TextBox
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents LblName As Label
+End Class
---
+++ HW3Objectss/HW3Objectss/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+
+    Dim TagSaleItems As New List(Of TagSaleItem)
+    Dim StoredPrice() As Double = {0.0, 0.0}
+    Dim total As Double = 0
+    Dim totalPrice As Double = 0
+    Dim average As Double = 0
+    Dim str As String
+    Dim str2 As String
+    Dim filePath As String = "C:\Users\Gared\source\VB\repos\HW3Objectss\HW3Objectss\stored.txt"
+
+    Private Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+
+        Dim newItem As New TagSaleItem()
+
+        If Not Double.TryParse(TxtInput.Text, newItem.ItemPrice) Then
+            MessageBox.Show("Please enter a valid price.")
+            Return
+        End If
+
+        newItem.ItemName = TxtName.Text
+
+        TagSaleItems.Add(newItem)
+
+        totalPrice += newItem.ItemPrice
+        average = totalPrice / TagSaleItems.Count
+
+        Update2()
+    End Sub
+
+    Private Sub BtnSave_Click(sender As Object, e As EventArgs) Handles BtnSave.Click
+
+        Dim sv As New System.IO.StreamWriter(filePath)
+        For Each item As TagSaleItem In TagSaleItems
+            sv.WriteLine(item.ItemName & "," & item.ItemPrice.ToString())
+        Next
+        sv.Close()
+        MessageBox.Show("Data saved to file.")
+
+    End Sub
+
+    Private Sub BtnLoad_Click(sender As Object, e As EventArgs) Handles BtnLoad.Click
+
+        If System.IO.File.Exists(filePath) Then
+            Dim lines() As String = System.IO.File.ReadAllLines(filePath)
+            TagSaleItems.Clear()
+            totalPrice = 0
+            For Each line As String In lines
+                If Not String.IsNullOrEmpty(line) Then
+                    Dim parts() As String = line.Split(",")
+                    If parts.Length = 2 Then
+                        Dim newItem As New TagSaleItem()
+                        newItem.ItemName = parts(0)
+                        Double.TryParse(parts(1), newItem.ItemPrice)
+                        TagSaleItems.Add(newItem)
+                        totalPrice += newItem.ItemPrice
+                    End If
+                End If
+            Next
+        End If
+
+        Update2()
+
+    End Sub
+
+    Private Sub Update2()
+        lblTotalSales.Text = totalPrice.ToString()
+        LblAverage.Text = average.ToString()
+        LblTotalItems.Text = TagSaleItems.Count.ToString()
+    End Sub
+
+End Class
+
+Public Class TagSaleItem
+
+    Private _itemPrice As Double
+    Private _itemName As String
+
+    Public Property ItemPrice As Double
+        Get
+            Return _itemPrice
+        End Get
+        Set(value As Double)
+            _itemPrice = value
+        End Set
+    End Property
+
+    Public Property ItemName As String
+        Get
+            Return _itemName
+        End Get
+        Set(value As String)
+            _itemName = value
+        End Set
+    End Property
+
+End Class
