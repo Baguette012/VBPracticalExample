9eee2de9d2fdf7f096e3037c645dd84460e3d27c
Baguette012 <137230559+Baguette012@users.noreply.github.com>
2023-06-24 16:27:02 -0400

    Add files via upload

---
+++ Hw1Arrays2/Hw1Arrays2/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+
+    Dim StoredPrice() As Double = {0.0, 0.0}
+    Dim total As Double = 0
+    Dim totalPrice As Double = 0
+    Dim average As Double = 0
+    Dim str As String
+    Dim str2 As String
+
+
+
+    Private Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+
+
+        total = total + 1
+        ReDim StoredPrice(total)
+        str = total.ToString
+        LblTotalItems.Text = str
+
+
+        str2 = TxtInput.Text
+        StoredPrice(total) = CDbl(str2)
+        totalPrice = CDbl(str2) + totalPrice
+
+
+        For index = 0 To StoredPrice.GetUpperBound(0)
+
+            lblTotalSales.Text = StoredPrice(index) + totalPrice
+
+
+            average = totalPrice / total
+            LblAverage.Text = average.ToString
+
+
+        Next
+
+
+
+
+    End Sub
+
+
+End Class
